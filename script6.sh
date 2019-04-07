2fd56969ca8a3c921ea6ac18de53b093037a3f1a
VladaLukovskaya <vladusik.l@yandex.ru>
2019-04-07 06:45:11 -0700

    add script6.sh and script7.sh

---
+++ script6.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p /home/suzen/Documents/projects/lab19/using/simple/bash/commands
+ls /home/suzen/Documents/projects/lab19/using/simple/bash/commands
---
+++ script7.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -r /home/john
+ls /home/suzen
